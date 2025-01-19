43c6dd840326f593dac10495943e67f530329fa4
MrSergio42 <sergijllepskij@gmail.com>
2025-01-19 13:25:03 +0200

    add music and labels

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ init()
 
 W = 500
 H = 700
-fps = 60
+FPS = 60
+
+life = 3
+kill = 0
+skiped = 0
+
+mixer.init()
+mixer.music.load("sounds/space.ogg")
+mixer.music.set_volume(0.3)
+mixer.music.play()
+
+fire_snd = mixer.Sound("sounds/fire.ogg")
+
+font.init()
+font1 = font.SysFont("fonts/Bebas_Neue_Cyrillic.ttf", 35, bold=True)
+font2 = font.SysFont("fonts/Bebas_Neue_Cyrillic.ttf", 20, bold=True)
 
 window = display.set_mode((W, H))
 display.set_caption("Shoter")
@@ -36,6 +51,10 @@ class Player(GameSprite):
         if keys_presed[K_d] and self.rect.x < W -  self.weight:
             self.rect.x += self.speed
 
+    def fire(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top, 20, "images/bullet.png", 15, 20)
+        bullets.add(bullet)
+
 class Enemy(GameSprite):
     def update(self):
         global kill
@@ -50,13 +69,21 @@ class Venom(GameSprite):
         super().__init__(x, y, speed, img, weight, height)
         self.angel = 0
         self.original_image = self.image
+
     def update(self):
         self.rect.y += self.speed
         self.angel += 2
         self.image = transform.rotate(self.original_image, self.angel)
+        self.rect = self.image.get_rect(center=self.rect.center)
         if self.rect.y > H - self.height:
             self.rect.x = randint(0, W - self.weight)
             self.rect.y = 0
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y < 0:
+            self.kill()
 
 player = Player(W / 2, H - 140, 5, "images/51fQqBomGhL-removebg-preview.png", 130, 140)
 enemis = sprite.Group()
@@ -66,16 +93,22 @@ for i in range(5):
 
 venom1 = Venom(randint(0, W - 70), randint(-35, 0), randint(1, 5), "images/asteroid.png", 70, 35)
 
-life = 3
-kill = 0
-skiped = 0
+venoms = sprite.Group()
+for i in range(3):
+    venom1 = Venom(randint(0, W - 70), randint(-35, 10), randint(1, 3), "images/asteroid.png", 70, 35)
+    venoms.add(venom1)
+
+bullets = sprite.Group() 
 
 game = True
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-
+        if e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_snd.play()
+                player.fire()
     window.blit(bg, (0, 0))
 
     player.draw()
@@ -87,6 +120,39 @@ while game:
     venom1.draw()
     venom1.update()
 
+    bullets.draw(window)
+    bullets.update()
 
-    display.flip()
-    clock.tick(fps)      
+
+
+    if sprite.groupcollide(bullets, enemis, True, True):
+        kill += 1
+        enemy = Enemy(randint(0, W - 70), randint(-35, 10), randint(1, 3), "images/ufo.png", 70, 35)
+        enemis.add(enemy)
+
+    if sprite.groupcollide(bullets, venoms, True, False):
+        pass
+
+    if sprite.spritecollide(player, venoms, True):
+        life -= 1
+        venom = Venom(randint(0, W - 70), randint(-35, 10), randint(1, 3), "images/asteroid.png", 70, 35)
+        venoms.add(venom)
+
+    if sprite.spritecollide(player, enemis, True):
+        life -= 1
+        enemy = Enemy(randint(0, W - 70), randint(-35, 10), randint(1, 3), "images/ufo.png", 70, 35)
+        enemis.add(enemy)
+
+    if life < 0:
+        game = False
+
+    skiped_txt = font1.render(f"Пропущено: {skiped}", True, (255, 255, 255))
+    kill_txt = font1.render(f"Вбито: {kill}", True, (255, 255, 255))
+    life_txt = font1.render(f"Життя: {life}", True, (255, 255, 255))
+
+    window.blit(skiped_txt, (10, 10))
+    window.blit(kill_txt, (10, 35))
+    window.blit(life_txt, (10, 60))
+
+    display.update()
+    clock.tick(FPS)
